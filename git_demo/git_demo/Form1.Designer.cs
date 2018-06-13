217097432c1724735d63fe641023c585fc7e76bf
Student <student@cse.local>
2018-06-12 20:47:02 -0400

    added label

--- git_demo/git_demo/Form1.Designer.cs
+++ git_demo/git_demo/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_name.Location = new System.Drawing.Point(51, 29);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(280, 134);
+            this.lbl_name.TabIndex = 0;
+            this.lbl_name.Text = "insert text here";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 291);
+            this.Controls.Add(this.lbl_name);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_name;
     }
 }
 
